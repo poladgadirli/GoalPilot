914b220d461d54ee0024e11b78eea80426ef507b
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 12:53:53 +0400

    init

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/AiPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.AIPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPlannerApplication.class, args);
+	}
+
+}
---
+++ AIPlanner/src/test/java/com/example/AIPlanner/AiPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
