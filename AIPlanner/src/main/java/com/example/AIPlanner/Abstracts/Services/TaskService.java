57ffcb6a3d2de885bb375cc65abbd717346d77f4
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 19:52:30 +0400

    Updated getAllTasks to pagination

--- AIPlanner/src/main/java/com/example/AIPlanner/Abstracts/Services/TaskService.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/Abstracts/Services/TaskService.java
@@ -3,12 +3,14 @@ package com.example.AIPlanner.Abstracts.Services;
 import com.example.AIPlanner.DTOs.Requests.Tasks.CreateTaskRequest;
 import com.example.AIPlanner.DTOs.Requests.Tasks.UpdateTaskRequest;
 import com.example.AIPlanner.DTOs.Responses.Tasks.TaskResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface TaskService {
 
-    List<TaskResponse> getAllTasks();
+    Page<TaskResponse> getAllTasks(Pageable pageable);
 
     TaskResponse getTaskById(Long id);
 
