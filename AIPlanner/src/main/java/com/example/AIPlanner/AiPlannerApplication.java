2cb5adeda86e05f9055588ed80ce766d2e396058
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 15:41:38 +0400

    ApiResponse

--- AIPlanner/src/main/java/com/example/AIPlanner/AiPlannerApplication.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/AiPlannerApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiPlannerApplication {
+public class AiPlannerApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiPlannerApplication.class, args);
