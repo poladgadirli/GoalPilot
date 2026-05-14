78837692b1f027162def9c01f7a90d5a055bc402
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 14:54:09 +0400

    CreateTaskRequest DTO

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/DTOs/Requests/Tasks/CreateTaskRequest.java
@@ -0,0 +1,23 @@
+package com.example.AIPlanner.DTOs.Requests.Tasks;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CreateTaskRequest
+{
+
+    @NotBlank(message = "Title boş ola bilməz")
+    @Size(max = 150, message = "Title maksimum 150 simvol ola bilər")
+    private String title;
+
+    @Size(max = 1000, message = "Description maksimum 1000 simvol ola bilər")
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
