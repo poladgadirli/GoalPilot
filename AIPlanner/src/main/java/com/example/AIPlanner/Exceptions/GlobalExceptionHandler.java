59190389b84464c0aa9b2aa6573d6e1570045510
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 15:37:33 +0400

    Updated GlobalExceptionHandler

--- AIPlanner/src/main/java/com/example/AIPlanner/Exceptions/GlobalExceptionHandler.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/Exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.example.AIPlanner.Exceptions;
 
 import com.example.AIPlanner.DTOs.Responses.Common.ErrorResponse;
+import com.example.AIPlanner.DTOs.Responses.Common.ValidationErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
-public class GlobalExceptionHandler
-{
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleTaskNotFound(TaskNotFoundException exception) {
@@ -18,5 +22,24 @@ public class GlobalExceptionHandler
         );
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationErrors(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        ValidationErrorResponse response = new ValidationErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation error",
+                errors
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
