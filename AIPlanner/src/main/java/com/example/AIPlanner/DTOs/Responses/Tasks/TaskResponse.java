729de6852d4f917abce97ecd7efcafc5d4f63dde
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 14:55:17 +0400

    TaskResponse DTO

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/DTOs/Responses/Tasks/TaskResponse.java
@@ -0,0 +1,41 @@
+package com.example.AIPlanner.DTOs.Responses.Tasks;
+
+import java.time.LocalDateTime;
+
+public class TaskResponse
+{
+
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt;
+
+    public TaskResponse(Long id, String title, String description, boolean completed, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
