755ebd281e6ecbe74b72624bee53e6d1c0d99ea3
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 23:43:33 +0400

    Created Category Entity

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Entities/Category.java
@@ -0,0 +1,76 @@
+package com.example.AIPlanner.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories" )
+public class Category
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String name;
+
+    @Column(length = 20)
+    private String color;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public Category()
+    {
+
+    }
+
+    public Category(Long id, String name, String color, LocalDateTime createdAt, LocalDateTime updatedAt)
+    {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
