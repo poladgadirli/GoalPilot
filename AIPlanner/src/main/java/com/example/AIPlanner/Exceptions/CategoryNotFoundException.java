b38aa2fcaf0dcf94b388ebaf7550bc5417cc39f0
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-14 23:54:08 +0400

    Created CategoryNotFoundException

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Exceptions/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.AIPlanner.Exceptions;
+
+public class CategoryNotFoundException extends RuntimeException
+{
+    public CategoryNotFoundException(Long id) {
+        super("Category not found with id: " + id);
+    }
+}
