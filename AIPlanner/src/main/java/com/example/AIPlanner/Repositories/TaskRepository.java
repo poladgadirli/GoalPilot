8a826276b1123cbddd63495b6865cf3eeb191fe3
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-16 14:47:07 +0400

    JpaSpecificationExecutor to TaskRepository

--- AIPlanner/src/main/java/com/example/AIPlanner/Repositories/TaskRepository.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/Repositories/TaskRepository.java
@@ -6,12 +6,13 @@ import com.example.AIPlanner.Enums.TaskStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
     @Query("""
     SELECT t FROM Task t
