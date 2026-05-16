e0015b58c7ee12dcbb33b98f2b7df0bfe4b27525
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-16 16:01:54 +0400

    Created BaseEntity

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Entities/Common/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.example.AIPlanner.Entities.Common;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity<T> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private T id;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
