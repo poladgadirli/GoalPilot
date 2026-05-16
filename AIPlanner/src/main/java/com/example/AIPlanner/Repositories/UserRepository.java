3453a4a857c7b558ba8c0610cf2342ca577939c7
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-16 19:13:54 +0400

    Created UserRepository

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.AIPlanner.Repositories;
+
+import com.example.AIPlanner.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmailOrUsername(String email, String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
