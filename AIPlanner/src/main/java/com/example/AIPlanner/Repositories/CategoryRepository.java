ed5dc49da283c7805a047e53b8d2cdf8364b4a41
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-16 23:36:36 +0400

    Updated CategoryRepository to userbased

--- AIPlanner/src/main/java/com/example/AIPlanner/Repositories/CategoryRepository.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/Repositories/CategoryRepository.java
@@ -3,16 +3,11 @@ package com.example.AIPlanner.Repositories;
 import com.example.AIPlanner.Entities.Category;
 import com.example.AIPlanner.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    Optional<Category> findByName(String name);
-
-    boolean existsByName(String name);
-
-    boolean existsByNameIgnoreCase(String name);
 
     List<Category> findByUser(User user);
 
