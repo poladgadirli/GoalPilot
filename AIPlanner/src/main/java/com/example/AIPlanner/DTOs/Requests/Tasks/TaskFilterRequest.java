b10bb0415514a16cc29e04a4bdfda093196a47f2
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-16 14:18:33 +0400

    Created TaskFilterRequest DTO

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/DTOs/Requests/Tasks/TaskFilterRequest.java
@@ -0,0 +1,44 @@
+package com.example.AIPlanner.DTOs.Requests.Tasks;
+
+import com.example.AIPlanner.Enums.TaskPriority;
+import com.example.AIPlanner.Enums.TaskStatus;
+
+public class TaskFilterRequest {
+
+    private Long categoryId;
+    private TaskStatus status;
+    private TaskPriority priority;
+    private Boolean completed;
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
