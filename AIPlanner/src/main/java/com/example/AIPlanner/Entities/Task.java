66e82306f4930a8ca4f8d922d91efaa145cd8061
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-16 22:44:05 +0400

    Task to user relation (get and sets)

--- AIPlanner/src/main/java/com/example/AIPlanner/Entities/Task.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/Entities/Task.java
@@ -40,6 +40,14 @@ public class Task extends BaseEntity<Long> {
     public Task() {
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public String getTitle() {
         return title;
     }
