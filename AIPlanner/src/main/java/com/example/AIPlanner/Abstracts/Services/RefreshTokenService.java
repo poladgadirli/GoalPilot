d3056e863768b9a15de495fdae7531c7036c65a4
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-20 14:08:51 +0400

    Created RefreshTokenService

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Abstracts/Services/RefreshTokenService.java
@@ -0,0 +1,13 @@
+package com.example.AIPlanner.Abstracts.Services;
+
+import com.example.AIPlanner.Entities.RefreshToken;
+import com.example.AIPlanner.Entities.User;
+
+public interface RefreshTokenService {
+
+    RefreshToken createRefreshToken(User user);
+
+    RefreshToken validateRefreshToken(String token);
+
+    void revokeRefreshToken(String token);
+}
