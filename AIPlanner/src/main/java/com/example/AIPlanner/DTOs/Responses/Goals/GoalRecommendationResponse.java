8c21f7e6ae4573b487cbba54fdbbdd858090797c
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-22 06:51:47 +0400

    Created GoalRecommendationResponse

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/DTOs/Responses/Goals/GoalRecommendationResponse.java
@@ -0,0 +1,40 @@
+package com.example.AIPlanner.DTOs.Responses.Goals;
+
+public class GoalRecommendationResponse {
+
+    private String goalTitle;
+    private Integer minimumRecommendedDays;
+    private Integer minimumRecommendedMinutes;
+    private String reason;
+
+    public GoalRecommendationResponse() {
+    }
+
+    public GoalRecommendationResponse(
+            String goalTitle,
+            Integer minimumRecommendedDays,
+            Integer minimumRecommendedMinutes,
+            String reason
+    ) {
+        this.goalTitle = goalTitle;
+        this.minimumRecommendedDays = minimumRecommendedDays;
+        this.minimumRecommendedMinutes = minimumRecommendedMinutes;
+        this.reason = reason;
+    }
+
+    public String getGoalTitle() {
+        return goalTitle;
+    }
+
+    public Integer getMinimumRecommendedDays() {
+        return minimumRecommendedDays;
+    }
+
+    public Integer getMinimumRecommendedMinutes() {
+        return minimumRecommendedMinutes;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+}
