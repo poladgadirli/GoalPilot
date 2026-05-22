0890d3116773eb03553beb45628880c37b51c194
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-22 12:41:04 +0400

    Created PlanTaskRepository

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Repositories/PlanTaskRepository.java
@@ -0,0 +1,16 @@
+package com.example.AIPlanner.Repositories;
+
+import com.example.AIPlanner.Entities.PlanTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlanTaskRepository extends JpaRepository<PlanTask, Long> {
+
+    List<PlanTask> findAllByPlanDayIdOrderByOrderIndexAsc(Long planDayId);
+
+    Optional<PlanTask> findByIdAndPlanDayId(Long id, Long planDayId);
+
+    void deleteAllByPlanDayId(Long planDayId);
+}
