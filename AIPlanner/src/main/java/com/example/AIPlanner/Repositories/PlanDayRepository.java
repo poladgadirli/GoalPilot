5585b7d7c865ca677730d78e31b72cc418a0c4f2
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-22 12:40:40 +0400

    Created PlanDayRepository

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Repositories/PlanDayRepository.java
@@ -0,0 +1,18 @@
+package com.example.AIPlanner.Repositories;
+
+import com.example.AIPlanner.Entities.PlanDay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlanDayRepository extends JpaRepository<PlanDay, Long> {
+
+    List<PlanDay> findAllByPlanIdOrderByDayNumberAsc(Long planId);
+
+    Optional<PlanDay> findByIdAndPlanId(Long id, Long planId);
+
+    boolean existsByPlanIdAndDayNumber(Long planId, Integer dayNumber);
+
+    void deleteAllByPlanId(Long planId);
+}
