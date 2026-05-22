6267f2d60195e3bb30dd2c7d4758fa308f95c037
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-22 16:00:19 +0400

    Created PlanTaskService

---
+++ AIPlanner/src/main/java/com/example/AIPlanner/Abstracts/Services/PlanTaskService.java
@@ -0,0 +1,10 @@
+package com.example.AIPlanner.Abstracts.Services;
+
+import com.example.AIPlanner.DTOs.Responses.Plans.PlanTaskResponse;
+
+public interface PlanTaskService {
+
+    PlanTaskResponse completeTask(Long taskId);
+
+    PlanTaskResponse uncompleteTask(Long taskId);
+}
