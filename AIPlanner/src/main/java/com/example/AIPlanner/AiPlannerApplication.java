b2f3641ce098a720a06003b05037deedb94772c4
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-23 17:08:49 +0400

    Updated AiPlannerApplication

--- AIPlanner/src/main/java/com/example/AIPlanner/AiPlannerApplication.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/AiPlannerApplication.java
@@ -2,13 +2,13 @@ package com.example.AIPlanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.AIPlanner.Configs.AiProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-public class AiPlannerApplication
-{
-
+@EnableConfigurationProperties(AiProperties.class)
+public class AiPlannerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AiPlannerApplication.class, args);
 	}
-
 }
