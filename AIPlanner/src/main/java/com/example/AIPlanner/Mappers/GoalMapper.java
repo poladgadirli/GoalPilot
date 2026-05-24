e326daa592c0400e0d5ead72dcdc4949475b0b34
Polad <206765139+poladgadirli@users.noreply.github.com>
2026-05-24 19:19:51 +0400

    Updated GoalMapper

--- AIPlanner/src/main/java/com/example/AIPlanner/Mappers/GoalMapper.java
+++ AIPlanner/src/main/java/com/example/AIPlanner/Mappers/GoalMapper.java
@@ -4,6 +4,7 @@ import com.example.AIPlanner.DTOs.Requests.Goals.CreateGoalRequest;
 import com.example.AIPlanner.DTOs.Requests.Goals.UpdateGoalRequest;
 import com.example.AIPlanner.DTOs.Responses.Goals.GoalResponse;
 import com.example.AIPlanner.Entities.Goal;
+import com.example.AIPlanner.Entities.GoalRecommendation;
 import com.example.AIPlanner.Entities.User;
 import com.example.AIPlanner.Enums.GoalStatus;
 import org.springframework.stereotype.Component;
@@ -11,20 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class GoalMapper {
 
-    public Goal toEntity(CreateGoalRequest request, User user) {
+    public Goal toEntity(CreateGoalRequest request, GoalRecommendation recommendation, User user) {
         Goal goal = new Goal();
 
-        goal.setTitle(request.getTitle().trim());
+        goal.setTitle(recommendation.getGoalTitle());
 
-        if (request.getDescription() != null) {
-            goal.setDescription(request.getDescription().trim());
+        if (recommendation.getGoalDescription() != null) {
+            goal.setDescription(recommendation.getGoalDescription());
         }
 
         goal.setStartDate(request.getStartDate());
         goal.setDurationDays(request.getDurationDays());
         goal.setDailyAvailableMinutes(request.getDailyAvailableMinutes());
-        goal.setMinimumRecommendedDays(request.getMinimumRecommendedDays());
-        goal.setMinimumRecommendedMinutes(request.getMinimumRecommendedMinutes());
+        goal.setMinimumRecommendedDays(recommendation.getMinimumRecommendedDays());
+        goal.setMinimumRecommendedMinutes(recommendation.getMinimumRecommendedMinutes());
         goal.setStatus(GoalStatus.ACTIVE);
         goal.setUser(user);
 
